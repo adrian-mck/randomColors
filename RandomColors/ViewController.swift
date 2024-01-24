51f4e2a4b512596f39c29541c16865f2c49a2474
Adrian Mück <adrian.mueck@student.fhws.de>
2024-01-24 20:33:55 +0100

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Adrian Mück on 24.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
